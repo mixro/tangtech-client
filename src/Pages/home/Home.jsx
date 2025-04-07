a471987b521553e35c10ec3d3576eb2e5ea2e5cf
i CEP <josephchongola43@gmail.com>
2025-04-07 13:10:45 +0300

    new file:   TANG TECH WEBSITE DOCUMENTANTION.pdf 	modified:   src/Pages/home/Home.jsx

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home-container">
-        <h1 className='not-seen'>Tang Tech & Engineering Ltd – Delivering cutting-edge electrical, ICT, security, and automation solutions. Trusted for innovation, reliability, and sustainable technology.</h1>
+        <p className='not-seen'>Tang Tech & Engineering Ltd – Delivering cutting-edge electrical, ICT, security, and automation solutions. Trusted for innovation, reliability, and sustainable technology.</p>
         <div className="home_gallery">
           <Swiper
               spaceBetween={0}
